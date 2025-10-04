1dc0df4dc5e00209a6411c23f79ec8dd249f3364
saadthedeveloper <m.saad23145@gmail.com>
2025-10-04 04:28:56 -0700

    Add parallax effect to hill1 and leaf

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 let mainHeading = document.querySelector("#main-heading");
 let hillRight = document.querySelector("#hill4");
 let hillLeft = document.querySelector("#hill3");
+let leaf = document.querySelector("#leaf");
+let hillback1 = document.querySelector("#hill1");
 
 
 
-window.addEventListener('scroll', ()=>{
+window.addEventListener('scroll', () => {
     let value = window.scrollY;
-    mainHeading.style.marginTop = value * 2.5 + 'px'; 
-    hillRight.style.marginRight = value * 0.3 + 'px'; 
-    hillLeft.style.marginLeft = value * 0.3 + 'px'; 
+    mainHeading.style.marginTop = value * 2.5 + 'px';
+    hillRight.style.marginRight = value * 0.3 + 'px';
+    hillLeft.style.marginLeft = value * 0.3 + 'px';
+    leaf.style.marginLeft = value * 1.3 + 'px';
+    hillback1.style.marginTop = value * 0.9 + 'px';
 
 
-} );
+});
 
